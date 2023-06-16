2250fd3a389e0b0747f551eaf9ea28abac7d84f0
junoshon <sanoldfox@naver.com>
2023-06-17 00:44:10 +0900

    iterate

---
+++ iterableInES6/es5iterate.js
@@ -0,0 +1,9 @@
+const list = [1,2,3];
+for(var i =0; i<list.length;i++){
+  console.log(list[i]);
+}
+
+const str = 'abc';
+for(var i =0;i<str.length;i++){
+  console.log(str[i]);
+}
---
+++ iterableInES6/es6iterate.js
@@ -0,0 +1,10 @@
+
+const list = [1,2,3];
+const str = 'abc';
+
+for(const a of list){
+  console.log(a);
+}
+for(const a of str){
+  console.log(a);
+}
